0ab12f9d208044af88536002ee7ae009ca767f2c
김재겸 <asbsds123@gmail.com>
2021-09-07 10:00:31 +0000

    relu, tanh 구현

--- DNN팀 2주차 자료/code/nn/functions/activations.py
+++ DNN팀 2주차 자료/code/nn/functions/activations.py
@@ -11,16 +11,33 @@ def sigmoid_diff(z):
 def none(z):
     return z
 
+def ReLU(z):
+    result = np.array([max(x,0) for x in z],dtype = np.float32)
+    return r
+
+def ReLU_diff(z):
+    result = np.array([int(x>0) for x in z],dtype = np.float32)
+    return result
+
+def tanh(z):
+    return 2*sigmoid(2*np.array(z)) - 1
+
+def tanh_diff(z):
+    return 1 - tanh(z)*tanh(z) 
 
 activations = {
     'none' : none,
-    'sigmoid': sigmoid
+    'sigmoid': sigmoid,
+    'tanh' : tanh,
+    'relu' : ReLU,
     
 }
 
 activations_diff = {
     'none' : none,
-    'sigmoid': sigmoid_diff
+    'sigmoid': sigmoid_diff,
+    'tanh' : tanh_diff,
+    'relu' : ReLU_diff,
 }
 
 class ActivationFunction:
