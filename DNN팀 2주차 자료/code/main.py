348b56b50f4fdfcc52db804a1c20e770adc685ca
김재겸 <asbsds123@gmail.com>
2021-09-08 09:26:37 +0000

    layer 로직 변경

--- DNN팀 2주차 자료/code/main.py
+++ DNN팀 2주차 자료/code/main.py
@@ -6,9 +6,9 @@ training_data, test_data = load_mnist.load_data()
 
 model = network.SequentialNetwork()
 
-model.set_input_layer(DenseLayer(784, 1 , 'none'))
-model.set_hidden_layer(DenseLayer(784, 196,'sigmoid'))
-model.set_output_layer(DenseLayer(196, 10,'sigmoid'))
+model.set_input_layer(DenseLayer(784 , 'none'))
+model.set_hidden_layer(DenseLayer(196,'sigmoid'))
+model.set_output_layer(DenseLayer(10,'sigmoid'))
 
 
 model.train(training_data, epochs=3, mini_batch_size=32,
