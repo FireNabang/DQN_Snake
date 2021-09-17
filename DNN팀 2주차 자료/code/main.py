b9ae3b25f1e542d8e81d14bae752d82ed1fb5e8d
worldi <jongha2788@u.sogang.ac.kr>
2021-09-17 14:39:03 +0900

    Conv Layer 초기 구현

--- DNN팀 2주차 자료/code/main.py
+++ DNN팀 2주차 자료/code/main.py
@@ -1,15 +1,18 @@
+from types import DynamicClassAttribute
 from nn import load_mnist
 from nn import network
 #from nn import Optimizer
 from nn.layers import DenseLayer
 
-training_data, test_data = load_mnist.load_data()
+import numpy as np
+if __name__ == '__main__' :
+    training_data, test_data = load_mnist.load_data()
+    
+    model = network.SequentialNetwork()
+    model.add_layer(DenseLayer(784,'none'))
+    model.add_layer(DenseLayer(392,'sigmoid'))
+    model.add_layer(DenseLayer(10,'sigmoid'))
 
-model = network.SequentialNetwork()
 
-model.add_layer(DenseLayer(784,'none'))
-model.add_layer(DenseLayer(392,'sigmoid'))
-model.add_layer(DenseLayer(10,'sigmoid'))
-#optimizer = Optimizer.Momentum();
-
-model.train(training_data, epochs=3, mini_batch_size=32, learning_rate=3.0, test_data=test_data)
+    #model.train(training_data, epochs=3, mini_batch_size=32, learning_rate=3.0, test_data=test_data)
+    
