d979aa6c80cb8a4b1b4bd149f836a80348d96225
김재겸 <asbsds123@gmail.com>
2021-10-05 14:45:51 +0000

    shape 수정

--- DNN/code/main.py
+++ DNN/code/main.py
@@ -2,12 +2,11 @@ from nn import load_mnist
 from nn import network
 from nn.layers import DenseLayer, Conv2DLayer, MaxPooling2DLayer, FlattenLayer,DropoutLayer
 
-
 def main():
-    training_data, test_data = load_mnist.load_data(train_data_cnt=500, test_data_cnt=1000, shape='matrix')
+    training_data, test_data = load_mnist.load_data(train_data_cnt=100, test_data_cnt=1000, shape='matrix')
     model = network.SequentialNetwork()
     model.add_layer(Conv2DLayer(input_size=(28, 28, 1)))
-    model.add_layer(Conv2DLayer(filter_count=10, filter_size=(2, 2)))
+    model.add_layer(Conv2DLayer(filter_count=10, filter_size=(3, 3)))
     model.add_layer(MaxPooling2DLayer(pool_size=(2, 2)))
     model.add_layer(DropoutLayer(0.5))
     model.add_layer(FlattenLayer())
@@ -20,7 +19,7 @@ def main():
     model.summary()
     model.train(training_data, epochs=3, mini_batch_size=10, learning_rate=0.9, test_data=test_data)
 
-    model.save_model()
+    # model.save_model()
 
 
 if __name__ == '__main__':
